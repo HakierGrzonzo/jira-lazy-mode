07fcc41591819c4a7672a911c106bbdddc838d76
Grzegorz Koperwas <grzes869@gmail.com>
2021-08-02 12:42:32 +0200

    Is working final?

--- jira-lazy-mode/__main__.py
+++ jira-lazy-mode/__main__.py
@@ -4,11 +4,14 @@ from requests import Session
 from requests.auth import HTTPBasicAuth
 import datetime
 from .secrets import Secrets
+from jinja2 import Template
 
 jira = JIRA(Secrets.jira_url, basic_auth=(Secrets.login, Secrets.password))
 session = Session()
 
-search_from = datetime.date.today() - datetime.timedelta(days=2)
+#search_from = datetime.date.today() - datetime.timedelta(days=40)
+search_from = datetime.datetime.strptime(input("Odkąd: "), "%Y-%m-%d").date() - datetime.timedelta(days=1)
+print("Searching from", search_from.isoformat())
 
 issues = jira.search_issues(
         r'worklogDate > {} and worklogDate < endOfDay() and worklogAuthor={}'.format(
@@ -20,10 +23,15 @@ issues = jira.search_issues(
     )
 
 logs = list()
-
+x = 1
 for issue in issues:
     try:
-        print("\33[2K{}".format(issue.fields.summary), end="\r")
+        print("\33[2K({}/{}) {}".format(
+                x,
+                len(issues),
+                issue.fields.summary
+            ), end="\r")
+        x += 1
         allWorklogs = session.get(
                 Secrets.jira_url + "/rest/api/2/issue/{}/worklog".format(
                     issue.key
@@ -47,10 +55,25 @@ logs.sort(key=lambda x: x[1])
 
 gaps = list()
 
+timeInDay = datetime.timedelta()
+timeCarry = datetime.timedelta()
+timeSum = datetime.timedelta()
+days = []
+hourSum = 0
+
 prevData = None
 for log, started_at, issue in logs:
     if prevData:
         if prevData["started_at"].date() != started_at.date():
+            hours = timeInDay.seconds // 3600
+            days.append({
+                "date": prevData["started_at"].date(),
+                "amount": hours
+            })
+            hourSum += hours
+            timeInDay -= datetime.timedelta(hours=hours)
+            timeSum += timeCarry
+            timeCarry = datetime.timedelta()
             print("----", started_at.date().isoformat(), "----", sep="\t")
         elif prevData["end_time"] != started_at:
             print(
@@ -62,12 +85,20 @@ for log, started_at, issue in logs:
     end_time = started_at + datetime.timedelta(
             seconds=log["timeSpentSeconds"]
         )
+    timeInDay += datetime.timedelta(
+            seconds=log["timeSpentSeconds"]
+        )
+    timeCarry += datetime.timedelta(
+            seconds=log["timeSpentSeconds"]
+        )
+
     prevData = {
         "started_at" : started_at,
         "end_time" : end_time,
         "log" : log,
         "issue" : issue
     }
+    """
     print(
             "{:<10.10}".format(issue.key),
             "{:<35.35}".format(issue.fields.summary.strip()),
@@ -79,7 +110,28 @@ for log, started_at, issue in logs:
             log["comment"].strip(), 
             sep="\t"
     )
-    
+"""
+hours = timeInDay.seconds // 3600
+days.append({
+    "date": prevData["started_at"].date(),
+    "amount": hours
+})
+print("Spent time", hours)
+hourSum += hours
+timeSum += timeCarry
+if timeCarry.seconds > 0:
+    print(timeCarry.seconds)
+    days[-1]["amount"] += 1
+    hourSum += 1
+print("Total", timeSum, ";", hourSum, hourSum * 27)
+
+template = Template(open("template.tex").read())
+result = template.render(lines=days, total = hourSum)
+resultFile = open("res.tex", "w+")
+resultFile.write(result)
+resultFile.close()
+
+"""
 repo = Repo("/home/hakiergrzonzo/Desktop/apa/weboweSO/WEBSO")
 assert not repo.bare
 if len(gaps) > 0:
@@ -101,4 +153,4 @@ if len(gaps) > 0:
             commit = commit.parents[0]
         else:
             break
-
+"""
